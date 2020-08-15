21fbb039ce3cff4e096e7740aa8b3a36d86dba8b
Kashdog <aneeshkashalikar@gmail.com>
2020-08-15 17:59:08 -0400

    Multiple Models

--- js/gridChart.js
+++ js/gridChart.js
@@ -282,12 +282,13 @@ populate_datasets().then(response =>{
             next = $(this).siblings(':first');
             }
             next.children(':first-child').clone().appendTo($(this));
-            
+            /*
             if (next.next().length>0) {
             next.next().children(':first-child').clone().appendTo($(this));
             } else {
                 $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
             }
+            */
         });
         show_gradients(selectors);
         
